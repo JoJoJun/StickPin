423eba42e3b1ed5fb8bed4cf3586593c0e640b6b
JoJoJun <mojinxinzhu@163.com>
2018-04-05 21:59:03 +0800

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour {
+
+    private Transform startPoint;//起始位置
+    private Transform spawnPoint;//屏幕外实例化针的位置
+    public GameObject pinPrefab;//整个针
+    private Pin currentPin;//当前针
+    private bool isGameOver = false;//是否游戏结束
+    private int score = 0;//分数
+    public Text scoreText;//显示分数的组件
+    private Camera mainCamera;//获得主场景的相机
+    public float animationSpeed=3 ;//gmeover动画速度
+    // Use this for initialization
+    void Start()
+    {
+        //获取具体位置
+        startPoint = GameObject.Find("StartPosition").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        mainCamera = Camera.main;
+        SpawnPin();
+        animationSpeed = 3;
+    }
+    //生成针：在spawnpoint位置实例化
+    void SpawnPin()
+    {
+        currentPin= GameObject.Instantiate(pinPrefab, spawnPoint.position, pinPrefab.transform.rotation).GetComponent<Pin>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver) return;//不响应鼠标点击了
+        //鼠标点击事件
+        if(Input.GetMouseButtonDown(0))
+        {
+            currentPin.StartFly();//从起始位置向上
+            score++;
+            scoreText.text = score.ToString();
+            SpawnPin();//重新实例化针
+        }
+    }
+    public void GameOver()
+    {
+        if (isGameOver) return;
+        
+        GameObject.Find("Circle").GetComponent<RotateSelf>().enabled = false;//停止旋转
+        StartCoroutine(GameOverAnimation());//游戏结束动画
+
+
+        isGameOver = true;
+    }
+    /// <summary>
+    /// unity协程是一个能暂停执行，暂停后立即返回，直到中断指令完成后继续执行的函数。
+
+   /// 它类似一个子线程单独出来处理一些问题，性能开销较小，但是他在一个MonoBehaviour提供的主线程里只能有一个处于运行状态的协程。
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator GameOverAnimation()
+    {
+        while(true)
+        {
+            
+            //lerp()进行差值计算。背景色和大小都线性变化
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, Color.red, animationSpeed* Time.deltaTime);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 4, animationSpeed * Time.deltaTime);
+            Debug.Log(animationSpeed);
+            if (Mathf.Abs(mainCamera.orthographicSize - 4) < 0.01f) break;
+            yield return 0;//每次暂停一帧
+        }
+        yield return new WaitForSeconds(0.2f);//动画暂停0.5秒
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//重新加载场景
+    }
+}
---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+    public float speed = 20;//针移动的速度
+    private bool isFly = false;//针是否在从起始点到圆的过程中
+    private bool isReach = false;//针是否实例化后到达了屏幕起始位置
+    private Transform startPoint;//屏幕上点击之前针的位置
+    private Transform circle;//获取组件大圆
+    private Vector3 targetCirclePosition;//由于不是插到圆心位置，需要另求目标位置
+	// Use this for initialization
+	void Start () {
+        startPoint = GameObject.Find("StartPosition").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetCirclePosition = circle.position;
+        targetCirclePosition.y -= 1.55f;//这里是理想碰撞下圆的y坐标和针的y坐标的差，也就是目标的插入位置
+
+    }
+    /// <summary>
+    /// 从起始点到插入，具体的移动见Update
+    /// </summary>
+	public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+	// Update is called once per frame
+	void Update () {
+		if(isFly == false)
+        {
+            if(isReach==false)
+            {//这一段是从屏幕外的生成位置，到屏幕上的开始位置，参数：当前位置，目标位置，速度
+                transform.position= Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)//计算距离
+                    isReach = true;//已到达起始位置
+            }
+        }
+        //从屏幕起始位置移动到圆
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePosition, speed * Time.deltaTime);
+            if(Vector3.Distance(transform.position,targetCirclePosition)<0.05f)
+            {
+                transform.position = targetCirclePosition;
+                transform.parent = circle;//这样就会和圆一起rotate
+                isFly = false;
+            }
+        }
+	}
+}
